61db93bb91c4d33aacf4c61850176d19ea23bc06
Deokgyu Yang <secugyu@gmail.com>
2021-03-29 12:01:57 +0900

    Implement Apple signing-in
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- lib/src/authentication/service/auth_service_impl.dart
+++ lib/src/authentication/service/auth_service_impl.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_oauth/firebase_auth_oauth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:crypto/crypto.dart';
 
 import '../../../main.dart';
 import '../constant/auth_type.dart';
@@ -85,19 +89,32 @@ class AuthServiceImpl implements AuthService {
         }
         break;
       case AuthType.apple:
-        // TODO: Edit webAuthenticationOptions parameters after obtaining proper keys
+        final charset =
+            '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+        final random = Random.secure();
+        final rawNonce =
+            List.generate(32, (_) => charset[random.nextInt(charset.length)])
+                .join();
+        final bytes = utf8.encode(rawNonce);
+        final digest = sha256.convert(bytes);
+        final nonce = digest.toString();
+
         final appleCredential = await SignInWithApple.getAppleIDCredential(
             scopes: [
               AppleIDAuthorizationScopes.email,
               AppleIDAuthorizationScopes.fullName,
             ],
-            webAuthenticationOptions:
-                WebAuthenticationOptions(clientId: null, redirectUri: null));
+            nonce: nonce,
+            webAuthenticationOptions: WebAuthenticationOptions(
+                clientId: "net.awesometic.ourEmojiChatting.signIn",
+                redirectUri: Uri.https("api.awesometic.net",
+                    "/our-emoji-chatting/apple-sign-in")));
 
         if (appleCredential != null) {
           final credential = OAuthProvider("apple.com").credential(
               accessToken: appleCredential.authorizationCode,
-              idToken: appleCredential.identityToken);
+              idToken: appleCredential.identityToken,
+              rawNonce: rawNonce);
 
           userCredential = await _repository.signInWithCredential(credential);
           firebaseUser = userCredential.user;
