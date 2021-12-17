f9926a224ab398213a853647cf5ac87df388a2af
Deokgyu Yang <secugyu@gmail.com>
2021-12-17 19:21:01 +0900

    pubspec: Use the local settings_ui package
    Since the same pacakge in pub.dev has lower version
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- lib/src/settings_screen/view/settings_view.dart
+++ lib/src/settings_screen/view/settings_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_settings_ui/flutter_settings_ui.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 import '../../utility/setting_option.dart';
 import '../../utility/string_const.dart';
