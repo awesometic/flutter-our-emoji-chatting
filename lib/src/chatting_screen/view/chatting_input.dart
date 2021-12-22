1311e1d1c72ad6174df5e03af9eb8914be2c40a4
Deokgyu Yang <secugyu@gmail.com>
2021-12-23 00:38:36 +0900

    lib: chatting_screen: Implement selecting a photo from the album
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- lib/src/chatting_screen/view/chatting_input.dart
+++ lib/src/chatting_screen/view/chatting_input.dart
@@ -1,7 +1,9 @@
 import 'dart:developer' as developer;
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../utility/auth_const.dart' show MessageType;
 import '../../utility/string_const.dart';
@@ -14,6 +16,8 @@ class ChattingInput extends StatelessWidget {
   final _textController = TextEditingController();
   final _focusNode = FocusNode();
 
+  File? _imageFile;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,7 +29,15 @@ class ChattingInput extends StatelessWidget {
               margin: const EdgeInsets.symmetric(horizontal: 1),
               child: IconButton(
                 icon: const Icon(Icons.image),
-                onPressed: () {},
+                onPressed: () async {
+                  var isSetImageSuccessfully = await _setImageFromPicker();
+
+                  if (isSetImageSuccessfully) {
+                    developer.log('Image is set successfully');
+                  } else {
+                    developer.log('Image is not set');
+                  }
+                },
                 color: ThemeConstant.primaryGreyColor,
               ),
             ),
@@ -99,5 +111,22 @@ class ChattingInput extends StatelessWidget {
 
     controller.clear();
     _focusNode.requestFocus();
+  }
+
+  Future<bool> _setImageFromPicker() async {
+    ImagePicker imagePicker = ImagePicker();
+    final pickedFile = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      _imageFile = File(pickedFile.path);
+
+      if (_imageFile != null) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
